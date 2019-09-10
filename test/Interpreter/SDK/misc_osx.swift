10e8d0f9d221d66304e229ef45c2c24a87471263
Luciano Almeida <passos.luciano@outlook.com>
2019-09-10 15:34:22 -0300

    update interpreter tests

--- test/Interpreter/SDK/misc_osx.swift
+++ test/Interpreter/SDK/misc_osx.swift
@@ -7,7 +7,7 @@ import CoreServices
 func testFSEventStreamRef(stream: FSEventStreamRef) {
   // FIXME: These should be distinct types, constructible from one another.
   // works by coincidence because both are currently OpaquePointer 
-  _ = stream as ConstFSEventStreamRef // expected-warning {{casting expression to 'ConstFSEventStreamRef' (aka 'OpaquePointer') doesn't change the type}} {{14-39=}}
+  _ = stream as ConstFSEventStreamRef // expected-warning {{casting expression of type 'FSEventStreamRef' (aka 'OpaquePointer') to 'ConstFSEventStreamRef' (aka 'OpaquePointer') doesn't change the type}} {{14-39=}}
   _ = ConstFSEventStreamRef(stream) // expected-error {{cannot invoke initializer for type 'ConstFSEventStreamRef' with an argument list of type '(FSEventStreamRef)'}}
   // expected-note @-1 {{overloads for 'ConstFSEventStreamRef' exist with these partially matching parameter lists:}}
 
